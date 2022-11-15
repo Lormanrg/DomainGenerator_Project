2e243cfd2aa262ae47debbcb8e115988f9a46afd
Lorman Romero <lormanrg@gmail.com>
2022-11-15 11:42:45 -0400

    updated

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// window.onload = function() {
+//   //write your code here
+
+
+
+//   console.log("Hello Rigo from the console!");
+// };
+let pronouns = ["the", "our"];
+let adjs = ["great", "big"];
+let nouns = ["jogger", "racoon"];
+
+let pronounIndex = Math.floor(Math.random() * pronouns.length);
+let adjIndex = Math.floor(Math.random() * adjs.length);
+let nounIndex = Math.floor(Math.random() * nouns.length);
+
+for (let pronounIndex of pronouns) {
+  for (let adjIndex of adjs) {
+    for (let nounIndex of nouns) {
+      console.log(`${pronounIndex}${adjIndex}${nounIndex}.com`);
+    }
+  }
+}
